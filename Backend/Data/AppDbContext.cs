04087c41d86ff0c97c57e26b22ae8dfa230970f9
Nathan Fontenele <nathangf60@outlook.com>
2024-10-04 00:42:47 -0300

    [DATABASE] Create database Users

--- Backend/Data/AppDbContext.cs
+++ Backend/Data/AppDbContext.cs
@@ -1,6 +1,22 @@
-﻿namespace Backend.Data;
+﻿using Backend.Model;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace Backend.Data;
+
+public class AppDbContext : DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+               
+    }
+
+    public DbSet<UserModel> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=App.db");
+        }
+    }
 }
